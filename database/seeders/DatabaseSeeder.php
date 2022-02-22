70ab2e3055aa6291091194f797c6728cda34f572
Muhammad Zulfahmi <mzulfahmi807@gmail.com>
2022-02-22 12:03:12 +0700

    add: sale seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -41,6 +41,7 @@ class DatabaseSeeder extends Seeder
             SpalSeeder::class,
             PurchaseSeeder::class,
             CoaSeeder::class,
+            SaleSeeder::class,
         ]);
     }
 }
