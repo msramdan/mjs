26b1001632adcaa3097c17db3fad5eb71ea46475
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-03-16 20:29:06 +0700

    passing uncheck timesheet to server

--- resources/views/sale/time_sheet/create.blade.php
+++ resources/views/sale/time_sheet/create.blade.php
@@ -297,15 +297,42 @@
             // $('#btn-back').prop('disabled', true)
             // $('#btn-back').text('Loading...')
             
-            let dataSerialized = $(this).serialize()
+            // include unchecklist checkbox to be inside form data
+            let formData = new FormData()
+            $('.form-check-timesheet-input').each((i) => {
+                if (!$('.form-check-timesheet-input').eq(i).is(':checked')) {
+                    formData.append('is_count[]', 'false')
+                } else {
+                    formData.append('is_count[]', 'true')
+                }
+            })
 
-            // check if is_count from dataSerialized is null, change it to false, else set it to true
-            dataSerialized = dataSerialized.replace(/is_count%5B%5D=null/g, 'is_count%5B%5D=false')
+            // serialize data then append to formData
+            let data = $(this).serializeArray()
+            data.forEach((item) => {
+
+                if(item.name != 'is_count[]') {
+                    formData.append(item.name, item.value)
+                }
+
+            })
+
+            formData.append('lama_waktu',$('#total-waktu-value').text())
+
+            // delete data with is_count[] == null from formData
+            formData.forEach((item, index) => {
+                if (item.name === 'is_count[]' && item.value == 'null') {
+                    formData.delete(index)
+                }
+            })
+            
 
             $.ajax({
                 url: '{{ route('time_sheet.store') }}',
-                type: 'post',
-                data: dataSerialized + '&lama_waktu=' + $('#total-waktu-value').text(),
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
                 success: function(res) {
                     console.log(res);
 
